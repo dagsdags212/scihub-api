42111b4df85a475861fc8ef3ba0d55116c26bb00
dagsdags <jegsamson.dev@gmail.com>
2024-04-12 04:28:39 +0800

    made 'raw' field the only require attribute for Citation class

--- scihub/classes/Article.py
+++ scihub/classes/Article.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
-from typing import Tuple
+from typing import Optional
+from pydantic import BaseModel, Field
 
 class Author(BaseModel):
     """Contains fields for representing an author."""
@@ -15,11 +15,14 @@ class Author(BaseModel):
 class Citation(BaseModel):
     """Contains fields for citing an article."""
     raw: str
-    title: str
-    publication_year: int
-    journal: str
-    doi: str
-    authors: list[str]
+    title: Optional[str | None]             = Field(default=None)
+    publication_year: Optional[int | None]  = Field(default=None)
+    journal: Optional[str | None]           = Field(default=None)
+    doi: Optional[str | None]               = Field(default=None)
+    authors: Optional[list[str]]            = Field(default_factor=list)
+
+    def __str__(self) -> str:
+        return self.raw
 
 class Article(BaseModel):
     """Contains article metadata."""
